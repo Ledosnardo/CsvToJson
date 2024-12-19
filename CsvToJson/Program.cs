66c26b8b7a1eb5a1e63401418ef763d664c213a2
ledosnardo <ledosnardo@gmail.com>
2024-12-18 23:25:54 -0300

    Adicionar arquivos de projeto.

---
+++ CsvToJson/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using System.Text.Json.Serialization;
+
+using Newtonsoft.Json;
+
+static void Main(string[] args)
+{
+	Console.Write("Digite o caminho completo do arquivo CSV: ");
+	string filePath = Console.ReadLine();
+
+	try
+	{
+		var csvData = ConvertCsvToJson(filePath);
+	}
+	catch (Exception ex)
+	{
+		Console.WriteLine($"Erro: {ex.Message}");
+	}
+
+	 string ConvertCsvToJson(string filePath)
+	{
+		var csvLines = File.ReadAllLines(filePath);
+
+		var headers = csvLines[0].Split(',');
+		var rows = new List< Dictionary<string, string> > ();
+
+		for(var i = 1; i < csvLines.Length; i++)
+		{
+			var values = csvLines[i].Split(",");
+			var rowDict = new Dictionary<string, string> ();
+
+			for(var j = 0; i < headers.Length;)
+			{
+				rowDict[headers[j]] = values[j];
+			}
+			rows.Add(rowDict);
+		}
+
+		return "";
+	}
+}
